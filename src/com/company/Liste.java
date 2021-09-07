b88b6eb7c7e4c5717633c9c3ba2055adafc601b5
Christian C <cph-cs450@cphbusiness.dk>
2021-09-07 11:43:51 +0200

    test af printFromTail

--- src/com/company/Liste.java
+++ src/com/company/Liste.java
@@ -7,28 +7,70 @@ public class Liste {
     Node tail = null;
 
 
-    public boolean isEmpty(){
-        if(head == null ){
+    public boolean isEmpty() {
+        if (head == null) {
             return true;
-        }else
+        } else
             return false;
     }
-public Node InsertFromHead(String s)
-{
-    Node node = new Node(s);
+
+    public Node InsertFromHead(String s) {
+        Node node = new Node(s);
 
 
-    if(isEmpty()){
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+            return head;
+        }
+        head.previous = node;
+        node.next = head;
         head = node;
-        tail = node;
+
         return head;
     }
-    head.previous =node;
-    node.next = head;
-    head = node;
-
-    return head;
-}
 
 
+    public String printFromHead() {
+
+        Node n = head;
+        String res = "";
+
+        while (n != null) {
+            res = res + n.data;
+            n = n.next;
+
+        }
+        return res;
+    }
+
+    public Node insertFromTail(String s) {
+
+        Node node = new Node(s);
+
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+            return tail;
+        }
+        tail.next = node;
+        node.previous = tail;
+        tail = node;
+
+        return tail;
+    }
+
+
+    public String printFromTail() {
+
+        Node n = tail;
+        String res = "";
+
+        while (n != null) {
+            res = n.data +res;
+            n = n.previous;
+
+        }
+        return res;
+    }
 }
--- src/com/company/ListeTest.java
+++ src/com/company/ListeTest.java
@@ -10,16 +10,41 @@ class ListeTest {
 
     @org.junit.jupiter.api.Test
     void isEmpty() {
-        assertEquals(true,liste.isEmpty());
+        assertEquals(true, liste.isEmpty());
         liste.InsertFromHead("Victor");
-        assertEquals(false,liste.isEmpty());
+        assertEquals(false, liste.isEmpty());
     }
-
 
 
     @org.junit.jupiter.api.Test
     void insertFromHead() {
-
+        assertEquals("1", liste.InsertFromHead("1").data);
     }
 
+    @Test
+    void printListFromHead() {
+        liste.InsertFromHead("1");
+        liste.InsertFromHead("2");
+        liste.InsertFromHead("3");
+
+        assertEquals("321", liste.printFromHead());
+    }
+
+    @Test
+    void insertFromTail() {
+
+
+
+assertEquals("111",liste.insertFromTail("111").data);
+    }
+
+    @Test
+    void printFromTail() {
+
+        liste.insertFromTail("1");
+        liste.insertFromTail("2");
+        liste.insertFromTail("3");
+
+        assertEquals("123",liste.printFromTail());
+    }
 }
